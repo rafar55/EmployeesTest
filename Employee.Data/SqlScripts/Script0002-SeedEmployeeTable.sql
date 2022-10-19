50d05a7cfa859ae7c1ad70e4f94806d9d9c36d61
Rafael Romero <rafaromero4407@gmail.com>
2022-10-19 11:41:24 -0600

    Updated connection string and seed script

--- Employee.Data/SqlScripts/Script0002-SeedEmployeeTable.sql
+++ Employee.Data/SqlScripts/Script0002-SeedEmployeeTable.sql
@@ -1,9 +1,9 @@
-INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES 
-('Rafael','Romero','+15184505651','04145','2016-08-28'),
-('Lula','Zamora','+13184505651','03145','2015-02-21'), 
-('Eliseo','Gomez','+16184505644','04145','2020-01-30'), 
-('Brandon','Hemingway','+13184505632','06745','2021-11-01'), 
-('Rick','Roberts','+12184505643','02445','2016-12-15'),
-('Dave','Plungion','+12184503251','02545','2008-07-16'), 
-('Jose','Heymans','+17184435651','02146','2018-09-27'), 
-('Vidal','Fuentes','+18184505651','02646','2019-10-22')
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Rafael','Romero','+15184505651','04145','2016-08-28')
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Lula','Zamora','+13184505651','03145','2015-02-21') 
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Eliseo','Gomez','+16184505644','04145','2020-01-30') 
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Brandon','Hemingway','+13184505632','06745','2021-11-01') 
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Rick','Roberts','+12184505643','02445','2016-12-15')
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Dave','Plungion','+12184503251','02545','2008-07-16') 
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Jose','Heymans','+17184435651','02146','2018-09-27') 
+INSERT INTO Employees(FirstName,LastName,Phone,ZipCode,HireDate) VALUES ('Vidal','Fuentes','+18184505651','02646','2019-10-22')
+GO
